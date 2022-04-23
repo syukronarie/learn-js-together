33379d59f3fa05c7b45081d2297509084e656473
syukronarie <syukronarie@gmail.com>
2022-04-23 16:56:03 +0700

    fix base url

--- react-apolloclient-subscription/src/libs/client/index.js
+++ react-apolloclient-subscription/src/libs/client/index.js
@@ -1,26 +1,23 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { split, HttpLink } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
-import { createClient } from "graphql-ws";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { split, HttpLink } from '@apollo/client';
+import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 
 const httpLink = new HttpLink({
-	uri: "https://62fkvm.sse.codesandbox.io/graphql",
+	uri: 'https://apolloservertodoapp.herokuapp.com/graphql'
 });
 
 const wsLink = new GraphQLWsLink(
 	createClient({
-		url: "wss://62fkvm.sse.codesandbox.io/graphql",
+		url: 'wss://apolloservertodoapp.herokuapp.com/graphql'
 	})
 );
 
 const splitLink = split(
 	({ query }) => {
 		const definition = getMainDefinition(query);
-		return (
-			definition.kind === "OperationDefinition" &&
-			definition.operation === "subscription"
-		);
+		return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
 	},
 	wsLink,
 	httpLink
@@ -28,7 +25,7 @@ const splitLink = split(
 
 const client = new ApolloClient({
 	link: splitLink,
-	cache: new InMemoryCache(),
+	cache: new InMemoryCache()
 });
 
 export default client;
